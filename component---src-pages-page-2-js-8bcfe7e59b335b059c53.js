ccb03cd5e852b752f4b813f6f4c516d106686029
Madan Kumar <kumarmadan.j@gmail.com>
2019-01-20 23:26:11 +0530

    Updates

---
+++ component---src-pages-page-2-js-8bcfe7e59b335b059c53.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{139:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(145),c=a(151),r=a(150);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-8bcfe7e59b335b059c53.js.map
